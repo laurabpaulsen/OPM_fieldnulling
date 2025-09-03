e2e5feb1384cd0331873d92dc90dc061ae9d6ae6
laurabpaulsen <202005791@post.au.dk>
2025-09-03 10:44:38 +0200

    change to page2

--- utils/sensorcontrol.py
+++ utils/sensorcontrol.py
@@ -300,7 +300,7 @@ class OPMQuspinControl:
 
         delimiter = delimiters.get(value)
 
-        for row in self.connections[8090]["page1"]:
+        for row in self.connections[8090]["page2"]:
             print(f"here is row: {row}")
     
             # check if there is a 1 or zero after the delimiter
